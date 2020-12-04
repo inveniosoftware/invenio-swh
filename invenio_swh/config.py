3d9caf06b5e29cdb656038b279313e1abe6ca31f
Alex Dutton <alex@cottagelabs.com>
2020-12-04 11:58:02 +0000

    Initial config layout
    This is a first iteration, and might need to be finessed later on.
    
    Resolves #7.

--- invenio_swh/config.py
+++ invenio_swh/config.py
@@ -8,11 +8,15 @@
 
 """Support for onward deposit of software artifacts to Software Heritage"""
 
-# TODO: This is an example file. Remove it if your package does not use any
-# extra configuration variables.
-
-INVENIO_SWH_DEFAULT_VALUE = 'foobar'
-"""Default value for the application."""
 
 INVENIO_SWH_BASE_TEMPLATE = 'invenio_swh/base.html'
 """Default base template for the demo page."""
+
+INVENIO_SWH_SERVICES = {
+    'swh': {
+        'NAME': "Software Heritage",
+        'SERVICE_DOCUMENT': 'https://deposit.softwareheritage.org/1/servicedocument/',
+        'USERNAME': None,
+        'PASSWORD': None,
+    }
+}
