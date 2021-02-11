a1013d6f0f42c0402ce72e8b26e5c8ecfa39ccd1
Alex Dutton <alex@cottagelabs.com>
2021-02-11 09:59:41 +0000

    Config pulled through to the extension object
    This also exposes the metadata class on the extension object

--- invenio_swh/ext.py
+++ invenio_swh/ext.py
@@ -8,36 +8,75 @@
 
 """Support for onward deposit of software artifacts to Software Heritage"""
 
-from flask_babelex import gettext as _
+import typing
+from flask import current_app
 
+import sword2
 from . import config
+from .metadata import SWHMetadata
 
 
 class InvenioSWH(object):
     """invenio-swh extension."""
 
+    extension_name = "invenio-swh"
+
+    url_config_key = "INVENIO_SWH_SERVICE_DOCUMENT"
+    collection_name_config_key = "INVENIO_SWH_COLLECTION_NAME"
+    collection_iri_config_key = "INVENIO_SWH_COLLECTION_IRI"
+    username_config_key = "INVENIO_SWH_USERNAME"
+    password_config_key = "INVENIO_SWH_PASSWORD"
+
+    metadata_cls: typing.Type[SWHMetadata] = SWHMetadata
+
     def __init__(self, app=None):
         """Extension initialization."""
-        # TODO: This is an example of translation string with comment. Please
-        # remove it.
-        # NOTE: This is a note to a translator.
-        _('A translation string')
         if app:
             self.init_app(app)
 
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
-        app.extensions['invenio-swh'] = self
+        app.extensions[self.extension_name] = self
 
     def init_config(self, app):
         """Initialize configuration."""
         # Use theme's base template if theme is installed
-        if 'BASE_TEMPLATE' in app.config:
+        if "BASE_TEMPLATE" in app.config:
             app.config.setdefault(
-                'INVENIO_SWH_BASE_TEMPLATE',
-                app.config['BASE_TEMPLATE'],
+                "INVENIO_SWH_BASE_TEMPLATE",
+                app.config["BASE_TEMPLATE"],
             )
         for k in dir(config):
-            if k.startswith('INVENIO_SWH_'):
+            if k.startswith("INVENIO_SWH_"):
                 app.config.setdefault(k, getattr(config, k))
+
+    @property
+    def sword_client(self) -> sword2.Connection:
+        if self.is_configured:
+            return sword2.Connection(
+                service_document_iri=current_app.config[self.url_config_key],
+                user_name=current_app.config[self.username_config_key],
+                user_pass=current_app.config[self.password_config_key],
+            )
+
+    @property
+    def collection_name(self) -> str:
+        return current_app.config[self.collection_name_config_key]
+
+    @property
+    def collection_iri(self) -> str:
+        return current_app.config[self.collection_iri_config_key]
+
+    @property
+    def is_configured(self) -> bool:
+        return bool(
+            current_app.config.get(self.url_config_key)
+            and current_app.config.get(self.collection_name_config_key)
+            and current_app.config.get(self.username_config_key)
+            and current_app.config.get(self.password_config_key)
+        )
+
+    @property
+    def metadata(self) -> SWHMetadata:
+        return self.metadata_cls(self)
