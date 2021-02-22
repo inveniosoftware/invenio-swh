214e55424b39b104557b7e67a9edacee2785dfa0
Alex Dutton <alex@cottagelabs.com>
2021-02-22 12:25:01 +0000

    First component tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,14 +11,26 @@
 See https://pytest-invenio.readthedocs.io/ for documentation on which test
 fixtures are available.
 """
+
 import json
 import os
+import tempfile
 
 import pytest
 from flask import Flask
 from flask_babelex import Babel
+from flask_principal import Identity, Need, UserNeed
+from flask_sqlalchemy import SQLAlchemy
+from invenio_access import InvenioAccess
+from invenio_files_rest import InvenioFilesREST
+from invenio_jsonschemas import InvenioJSONSchemas
+from invenio_rdm_records import InvenioRDMRecords
+from invenio_rdm_records.services import RDMRecordService
+from invenio_rdm_records.services.config import RDMRecordServiceConfig
+from invenio_records import InvenioRecords
 
 from invenio_swh import InvenioSWH
+from invenio_swh.components import InvenioSWHComponent
 from invenio_swh.views import blueprint
 
 
@@ -32,16 +44,39 @@ def celery_config():
 
 
 @pytest.fixture(scope='module')
-def create_app(instance_path):
+def create_app(instance_path, entry_points):
     """Application factory fixture."""
-    def factory(**config):
+
+    def app_factory(**config):
         app = Flask('testapp', instance_path=instance_path)
-        app.config.update(**config)
+
+        app.config.update(
+            **config,
+
+            # SQLALCHEMY_DATABASE_URI=os.environ.get(
+            #     'SQLALCHEMY_DATABASE_URI', 'sqlite:///' + db_filename),
+            # TESTING=True,
+            JSONSCHEMAS_ENDPOINT='/schemas/',
+            JSONSCHEMAS_HOST='localhost',
+
+            INVENIO_SWH_COLLECTION_IRI='http://swh.invalid/'
+        )
+
         Babel(app)
+        SQLAlchemy(app)
+
         InvenioSWH(app)
+        InvenioAccess(app)
+        InvenioRecords(app)
+        InvenioRDMRecords(app)
+        InvenioJSONSchemas(app)
+        InvenioFilesREST(app)
+
         app.register_blueprint(blueprint)
+
         return app
-    return factory
+
+    return app_factory
 
 
 @pytest.fixture()
@@ -53,3 +88,29 @@ def example_record():
         )
     ) as f:
         return json.load(f)
+
+
+class RDMWithSWHRecordServiceConfig(RDMRecordServiceConfig):
+    """A record service config with our SWH component."""
+
+    indexer_cls = lambda *args, **kwargs: None
+
+    components = [
+        *RDMRecordServiceConfig.components,
+        InvenioSWHComponent,
+    ]
+
+
+@pytest.fixture
+def rdm_with_swh_record_service():
+    """Return a record service with an SWH component plugged in."""
+    return RDMRecordService(config=RDMWithSWHRecordServiceConfig)
+
+
+@pytest.fixture(scope="module")
+def identity_simple():
+    """Simple identity fixture."""
+    i = Identity(1)
+    i.provides.add(UserNeed(1))
+    i.provides.add(Need(method='system_role', value='any_user'))
+    return i
--- tests/test_component.py
+++ tests/test_component.py
@@ -1,2 +1,42 @@
-def test_create_draft():
-    pass
+import unittest.mock
+
+import pytest
+from flask import Flask
+from flask_principal import Identity
+from invenio_rdm_records.services import RDMRecordService
+
+from invenio_swh import InvenioSWH
+from invenio_swh.components import InvenioSWHComponent
+
+
+def test_create_draft_non_software(
+    rdm_with_swh_record_service: RDMRecordService,
+    identity_simple: Identity,
+    base_app: Flask,
+    database,
+    location,
+):
+    with unittest.mock.patch.object(InvenioSWH, 'sword_client') as sword_client:
+        record_item = rdm_with_swh_record_service.create(identity_simple, {}, None)
+        assert not sword_client.create.called
+        with pytest.raises(KeyError):
+            _ = record_item._record['ext'][InvenioSWHComponent.internal_ext_key]
+
+
+def test_create_draft_software(
+    rdm_with_swh_record_service: RDMRecordService,
+    identity_simple: Identity,
+    example_record,
+    base_app: Flask,
+    database,
+    location,
+):
+    with unittest.mock.patch.object(InvenioSWH, 'sword_client') as sword_client:
+        sword_client.create.return_value.edit = 'http://sword.invalid/edit-iri'
+        sword_client.create.return_value.edit_media = 'http://sword.invalid/edit-media-iri'
+        sword_client.create.return_value.se_iri = 'http://sword.invalid/se-iri'
+
+        record_item = rdm_with_swh_record_service.create(identity_simple, example_record, None)
+        assert sword_client.create.called
+
+        internal_ext_data = record_item._record['ext'][InvenioSWHComponent.internal_ext_key]
