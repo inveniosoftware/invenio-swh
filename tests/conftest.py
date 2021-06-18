c539d915e399b7e322fee321af39dd902f79efc2
Alex Dutton <alex@cottagelabs.com>
2021-06-18 13:10:31 +0100

    Update conftest for upstream changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,7 @@ fixtures are available.
 import json
 import os
 import unittest.mock
+import uuid
 
 import pytest
 from flask import Flask
@@ -22,16 +23,39 @@ from flask_babelex import Babel
 from flask_principal import Identity, Need, UserNeed
 from flask_sqlalchemy import SQLAlchemy
 from invenio_access import InvenioAccess
+from invenio_access.permissions import system_process
 from invenio_files_rest import InvenioFilesREST
 from invenio_jsonschemas import InvenioJSONSchemas
+from invenio_records_resources import InvenioRecordsResources
+from invenio_records_resources.registry import ServiceRegistry
+from invenio_records_resources.services import FileService
+from invenio_search import InvenioSearch
+# from invenio_app.factory import create_app as _create_app
+
 from invenio_rdm_records import InvenioRDMRecords
 from invenio_rdm_records.services import RDMRecordService
-from invenio_rdm_records.services.config import RDMRecordServiceConfig
+from invenio_rdm_records.services.config import RDMFileDraftServiceConfig, \
+    RDMRecordServiceConfig
 from invenio_records import InvenioRecords
+
+from invenio_vocabularies import InvenioVocabularies
+from invenio_vocabularies.proxies import current_service as vocabulary_service
 
 from invenio_swh import InvenioSWH
 from invenio_swh.components import InvenioSWHComponent
 from invenio_swh.views import blueprint
+
+# the primary requirement for the system user's ID is to be unique
+# the IDs of users provided by Invenio-Accounts are positive integers
+# the ID of an AnonymousIdentity from Flask-Principle is None
+# and the documentation for Flask-Principal makes use of strings for some IDs
+system_user_id = "system"
+"""The user ID of the system itself, used in its Identity."""
+
+system_identity = Identity(system_user_id)
+"""Identity used by system processes."""
+
+system_identity.provides.add(system_process)
 
 
 @pytest.fixture(scope="module")
@@ -66,15 +90,29 @@ def create_app(instance_path, entry_points):
         InvenioSWH(app)
         InvenioAccess(app)
         InvenioRecords(app)
+        InvenioRecordsResources(app)
         InvenioRDMRecords(app)
         InvenioJSONSchemas(app)
         InvenioFilesREST(app)
+        InvenioSearch(app)
+        InvenioVocabularies(app)
 
         app.register_blueprint(blueprint)
 
         return app
 
     return app_factory
+
+
+@pytest.fixture(scope='module')
+def app_config(app_config):
+    app_config['RECORDS_REFRESOLVER_CLS'] = \
+        "invenio_records.resolver.InvenioRefResolver"
+    app_config['RECORDS_REFRESOLVER_STORE'] = \
+        "invenio_jsonschemas.proxies.current_refresolver_store"
+    # Variable not used. We set it to silent warnings
+    app_config['JSONSCHEMAS_HOST'] = 'not-used'
+    return app_config
 
 
 @pytest.fixture()
@@ -91,10 +129,12 @@ class RDMWithSWHRecordServiceConfig(RDMRecordServiceConfig):
 
     indexer_cls = unittest.mock.Mock
 
-    components = [
-        *RDMRecordServiceConfig.components,
-        InvenioSWHComponent,
-    ]
+    components = RDMRecordServiceConfig.components
+    if InvenioSWHComponent not in components:
+        components = [
+            *RDMRecordServiceConfig.components,
+            InvenioSWHComponent,
+        ]
 
 
 @pytest.fixture
@@ -111,3 +151,47 @@ def identity_simple():
     i.provides.add(Need(method="system_role", value="authenticated_user"))
     i.provides.add(Need(method="system_role", value="any_user"))
     return i
+
+
+@pytest.fixture(scope="module")
+def resource_type_type(app):
+    """Resource type vocabulary type."""
+    return vocabulary_service.create_type(
+        system_identity, "resource_types", "rsrct")
+
+
+@pytest.fixture(scope="module")
+def resource_type_software(app, resource_type_type):
+    """Resource type vocabulary record."""
+    return vocabulary_service.create(system_identity, {
+        "id": "software",
+        "props": {
+            "csl": "graphic",
+            "datacite_general": "Image",
+            "datacite_type": "Photo",
+            "openaire_resourceType": "25",
+            "openaire_type": "dataset",
+            "schema.org": "https://schema.org/Photograph",
+            "subtype": "image-photo",
+            "subtype_name": "Photo",
+            "type": "image",
+            "type_icon": "chart bar outline",
+            "type_name": "Image",
+        },
+        "title": {
+            "en": "Software"
+        },
+        "type": "resource_types"
+    })
+
+
+@pytest.yield_fixture()
+def files_service(app):
+    files_service = FileService(RDMFileDraftServiceConfig)
+    registry: ServiceRegistry = app.extensions["invenio-records-resources"].registry
+    # Let's pick a random service_id…
+    registry.register(files_service, str(uuid.uuid4()))
+    try:
+        yield files_service
+    finally:
+        del registry._services[registry.get_service_id(files_service)]
