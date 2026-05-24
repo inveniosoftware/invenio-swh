a3ae1fde7270cb3820d4dc5822e07f94b53bd9eb
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-05-24 22:31:43 +0200

    chore(licenses): update license headers to use SPDX

--- invenio_swh/__init__.py
+++ invenio_swh/__init__.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2020-2025 CERN.
-# Copyright (C) 2020 Cottage Labs LLP.
-#
-# Invenio-SWH is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# SPDX-FileCopyrightText: 2020-2025 CERN.
+# SPDX-FileCopyrightText: 2020 Cottage Labs LLP.
+# SPDX-License-Identifier: MIT
 """Invenio-SWH extension."""
 
 from .ext import InvenioSWH
